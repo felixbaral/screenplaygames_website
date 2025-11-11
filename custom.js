f0143c46457cf42cb3b3a7449abfe42d68fd8aba
felix baral <felixbaral@me.com>
2025-11-11 02:41:46 +0100

    good state

--- custom.js
+++ custom.js
@@ -5,10 +5,10 @@ import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 
 var centerContainer = null;
 var centerCamera = null;
+var cameraCamera = null;
 var centerRenderer = null;
 var centerScene = null;
 var topContainer = null;
-var topCamera = null;
 var topRenderer = null;
 var topScene = null;
 
@@ -91,13 +91,86 @@ function onWindowResize() {
   centerRenderer.setSize(width, height);
   centerRenderer.render(centerScene, centerCamera);
 
-  width = topContainer.clientWidth;
-  height = topContainer.clientHeight;
-  topCamera.aspect = width / height;
-  topCamera.updateProjectionMatrix();
-  topRenderer.setSize(width, height);
-  topRenderer.render(topScene, topCamera);
+  cameraCamera.aspect = width / height;
+  cameraCamera.updateProjectionMatrix();
+  centerRenderer.setSize(width, height);
+  centerRenderer.render(centerScene, cameraCamera);
+}
+
+function setupCameraContainer() {
+  // Set size and append to container
+  const container = document.getElementById("camera-container");
+  topContainer = container;
+
+  // Scene, camera, renderer
+  var scene = new THREE.Scene();
+  topScene = scene;
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    container.clientWidth / container.clientHeight,
+    0.1,
+    1000,
+  );
+  cameraCamera = camera;
+  const renderer = new THREE.WebGLRenderer();
+  topRenderer = renderer;
+
+  renderer.setSize(container.offsetWidth, container.offsetHeight);
+  container.appendChild(renderer.domElement);
+
+  // style
+  renderer.setClearColor(0xffffff, 0);
+
+  const loader = new GLTFLoader();
+  var cameraModel = null;
+  loader.load(
+    "camera.glb",
+    function (gltf) {
+      cameraModel = gltf.scene;
+      scene.add(cameraModel);
+
+      const width = container.clientWidth * 0.02;
+      cameraModel.scale.set(width, width, width);
+
+      console.log(cameraModel.position);
+
+      cameraModel.rotation.x = Math.PI * 1.5;
+      cameraModel.rotation.y = Math.PI;
+
+      camera.lookAt(
+        cameraModel.position.x,
+        cameraModel.position.y,
+        cameraModel.position.z,
+      );
+      animateCamera();
+      renderer.render(scene, camera);
+    },
+    undefined,
+    function (error) {
+      console.error(error);
+    },
+  );
+
+  const light = new THREE.AmbientLight(0xffffff); // soft white light
+  scene.add(light);
+
+  // Position camera
+  camera.position.z = 7;
+
+  // Render loop
+  function animateCamera() {
+    requestAnimationFrame(animateCamera);
+    renderer.render(scene, camera);
+  }
+
+  // controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 0, 0);
+  controls.update();
+  controls.enablePan = false;
+  controls.enableDamping = true;
+  controls.enableZoom = false;
 }
 
 setupTapeContainer();
-//setupHeaderContainer();
+setupCameraContainer();
